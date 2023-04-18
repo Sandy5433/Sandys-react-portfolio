63f1c968ef5aa9ee727209ffa5b0137e042c9152
Sandy Hung <sandyhung83@yahoo.com.tw>
2023-04-18 20:47:19 +0930

    updated contact page input verification

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,105 +2,88 @@ import React, { useState } from 'react';
 import { validateEmail } from '../utils/helpers';
 
 function Contact() {
-  // Create state variables for the fields in the form
-  // Setting their initial values to an empty string
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [message, setMessage] = useState('');
-  const [errorMessage, setErrorMessage] =useState('')
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
 
-  const handleInputChange = (e) => {
-    // Getting the value and field of the input which triggered the change
-    const { target } = e;
-    const inputType = target.name;
-    const inputValue = target.value;
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
 
-    // Based on the input type, we set the state of either email, name, and message
-    if (inputType === 'email') {
-      setEmail(inputValue);
-    } else if (inputType === 'name') {
-      setName(inputValue);
-    } else {
-      setMessage(inputValue);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log('Submit Form', formState);
     }
   };
 
-  const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    e.preventDefault();
-
-    //check if the name is empty. If so we set an error message to be displayed on the page.
-    if (!name) {
-        setErrorMessage('*Name is required');
-        return;
+  const handleChange = (e) => {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
       }
-    if (!validateEmail(email)) {
-      setErrorMessage('*Email address is invalid');
-      //exit out of this code block if something is wrong so that the user can correct it
-      return;
-      // Then check to see if the message is empty. If so, we set an error message regarding the message.
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
     }
-    if (!message) {
-      setErrorMessage('*Message is required');
-      return;
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+      console.log('Handle Form', formState);
     }
-    ;
-
-    // If everything goes according to plan, we want to clear out the input after a successful registration.
-    setName('');
-    setMessage('');
-    setEmail('');
-    setErrorMessage('');
   };
 
   return (
-    <div>
+    <section>
       <h3 className='page-title'>Contact</h3>
-      <form className="form">
-      <div className="field">
-      <label>Name:</label>
-      </div>
-      <div className="input">
-      <input
-          value={name}
-          name="name"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Your Name"
-        />
-        </div>
+      <form className="form" id="contact-form" onSubmit={handleSubmit}>
         <div className="field">
-        <label>Email address:</label>
+          <label htmlFor="name">Name:</label>
         </div>
         <div className="input">
-        <input
-          value={email}
-          name="email"
-          onChange={handleInputChange}
-          type="email"
-          placeholder="sample@email.com"
-        />
+          <input
+            type="text"
+            name="name"
+            defaultValue={name}
+            onBlur={handleChange}
+          />
         </div>
         <div className="field">
-        <label>Message:</label>
+          <label htmlFor="email">Email address:</label>
         </div>
         <div className="input">
-        <input
-          value={message}
-          name="message"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Type a message"
-        />
+          <input
+            type="email"
+            name="email"
+            defaultValue={email}
+            onBlur={handleChange}
+          />
         </div>
-        <button type="button" onClick={handleFormSubmit}>Submit</button>
+        <div className="field">
+          <label htmlFor="message">Message:</label>
+        </div>
+        <div className="input">
+          <textarea
+            name="message"
+            rows="5"
+            defaultValue={message}
+            onBlur={handleChange}
+          />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button type="submit">Submit</button>
       </form>
-      {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage}</p>
-        </div>
-      )}
-    </div>
+    </section>
   );
 }
 
